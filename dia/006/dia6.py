df4a33d91f4ffd58be720f0a1800582c6aa62016
weverton henrique <wevertonwhm18@outlook.com>
2025-01-30 19:12:23 -0300

    1

---
+++ dia/006/dia6.py
@@ -0,0 +1,10 @@
+# Lista de Convidados
+
+convidado = ["Joao", "Maria", "Pedro", "Bruno", "Rosa"]
+
+for i, convidado in enumerate(convidado):
+    print("Permitido acesso: ", convidado)
+
+# Estatísticas de Números
+
+
