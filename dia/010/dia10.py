6e1ca15b8dd1ff0e5b32f2c2671956072cccdfcc
weverton henrique <wevertonwhm18@outlook.com>
2025-04-09 22:45:58 -0300

    voltando

---
+++ dia/010/dia10.py
@@ -0,0 +1,20 @@
+#Crie um programa que pede uma frase ao usuário e conta quantas vogais existem nela.
+
+frase = input("Digite uma frase: ").lower()
+vogais = 'aeiou'
+contador = 0
+
+for letra in frase:
+    if letra in vogais:
+        contador +=1
+
+print(f"A frase digitada tem {contador} vogais.")
+
+decoracao = "--" * 7
+print(decoracao)
+
+
+frases = input("Difite uma frase:  ")
+palavras = frases.split()
+frase_invertida = " " .join(reversed(palavras))
+print(f"Frase invertida: {frase_invertida}")
