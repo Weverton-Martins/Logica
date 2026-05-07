5cffd2bdeb53d27be6a68d89d8f35a44017ab2e9
weverton henrique <wevertonwhm18@outlook.com>
2026-05-07 13:59:20 -0300

    Update dia8.py

--- dia/008/dia8.py
+++ dia/008/dia8.py
@@ -67,4 +67,74 @@ else:
 '''
 #3. Conversor de Temperaturas
 '''Crie funções para converter temperaturas entre Celsius, Fahrenheit e Kelvin.'''
+'''
+def celcius_kelvin(c):
+    kelvin = c + 273.15
+    return kelvin
+
+def kelvin_celcius(k):
+    celcius = k - 273.15
+    return celcius
+
+def celcius_fahrenheit(c):
+    fahrenheit = (c * 1.8) + 32
+    return fahrenheit
+
+def fahrenheit_celcius(f):
+    celcius = (f - 32)/1.8
+    return celcius
+
+def fahrenheit_kelvins(f):
+    kelvins = ((f-32)/1.8) + 273.15
+    return kelvins
+
+def kelvin_fahrenheit(k):
+    fahrenheit = (k-273.15)*1.8 + 32
+    return fahrenheit
+
+valor = float(input('Informe uma temperatura: '))
+opcao = input('Qual seu tipo de temepratura informada (Celsius, Fahrenheit ou Kelvin): ').lower()
+
+if opcao == 'celsius':
+    result = celcius_kelvin(valor)
+    print(f'A temperatura {valor} em Celsius para Kelvin: {result}')
+    result = celcius_fahrenheit(valor)
+    print(f'A temperatura {valor} em Celsius para Fahrenheit: {result}')
+
+elif opcao == 'fahrenheit':
+    result = fahrenheit_celcius(valor)
+    print(f'A temperatura {valor} em Fahrenheit para Celcius: {result}')
+    result = fahrenheit_kelvins(valor)
+    print(f'A temperatura {valor} em Fahrenheit para Kelvin: {result}')
+
+elif opcao == 'kelvin':
+    result = kelvin_fahrenheit(valor)
+    print(f'A temperatura {valor} em Kelvin para Fahrenheit: {result}')
+    result = kelvin_celcius(valor)
+    print(f'A temperatura {valor} em Kelvin para Celcius: {result}')
+
+else:
+    print('Tipo invalido de temperatura..')'''
+
+#4. Função Recursiva para Fatorial
+'''Crie uma função recursiva para calcular o fatorial de um número.'''
+
+def fatorial(n):
+    if n == 0 or n ==1:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+num = int(input('Informe o numero que deseja saber o fatorial: '))
+if num >=0:
+    result = fatorial(num)
+    print(f'O fatorial de {num} é {result}')
+else:
+    print('Numero invalido!')
+
+#Desafios
+#1. Gerador de Senhas Aleatórias
+'''Crie uma função que gera uma senha aleatória com tamanho especificado, contendo letras
+maiúsculas, minúsculas, números e símbolos.'''
+
 
