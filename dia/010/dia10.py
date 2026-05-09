edf9eff2dd8620b5ffcb11334e53558e4574857e
weverton henrique <wevertonwhm18@outlook.com>
2026-05-09 18:16:07 -0300

    Update dia10.py

--- dia/010/dia10.py
+++ dia/010/dia10.py
@@ -1,21 +1,2 @@
 #Crie um programa que pede uma frase ao usuário e conta quantas vogais existem nela.
 
-frase = input("Digite uma frase: ").lower()
-vogais = 'aeiou'
-contador = 0
-
-for letra in frase:
-    if letra in vogais:
-        contador +=1
-
-print(f"A frase digitada tem {contador} vogais.")
-
-decoracao = "--" * 7
-print(decoracao)
-
-
-# Escreva um programa que inverte a ordem das palavras em uma frase.
-frases = input("Difite uma frase:  ")
-palavras = frases.split()
-frase_invertida = " " .join(reversed(palavras))
-print(f"Frase invertida: {frase_invertida}")
