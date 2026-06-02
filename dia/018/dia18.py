e9745bbf9026a51997005f7efb0fa248b3d8cd01
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-01 21:24:08 -0300

    Update dia18.py

--- dia/018/dia18.py
+++ dia/018/dia18.py
@@ -1,13 +1,13 @@
 #Teste e Debugging 
 
 #Crie uma função que multiplica dois números, mas introduza um erro intencional. Use técnicas de depuração para identificar e corrigir o erro.
-import pdb
+'''import pdb
 def multiplicar(a,b):
     pdb.set_trace()
     return a * c # Erro: variável 'c' não definida
 
 resultado= multiplicar(4,5)
-print(resultado)
+print(resultado)'''
 
 #Crie uma função que soma dois números e escreva testes unitários para verificar seu funcionamento.
 import unittest
@@ -15,13 +15,13 @@ def somar(a,b):
     return a + b
 
 class TestSomar(unittest.TestCase):
-    def Test_somar_positivos(self):
+    def test_somar_positivos(self):
         self.assertEqual(somar(2, 3), 5)
 
-    def Test_somar_negativos(self):
+    def test_somar_negativos(self):
         self.assertEqual(somar(-1, -1), -2)
     
-    def Test_somar_zero(self):
+    def test_somar_zero(self):
         self.assertEqual(somar(0, 5), 5)
 
 if __name__ == '__main__':
